41cd9392588da08d61f00024b32d66c5838548f9
sari-rahul <sarisivadas93@gmail.com>
2024-09-18 10:03:52 +0200

    Add basic structure

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import Header from './components/Header/Header';
 import SideBar from './components/SideBar/SideBar';
 import { Container } from 'react-bootstrap';
 import HomeScreen from './screens/HomeScreen/HomeScreen';
+import './_app.scss';
 
 const App = () => {
   return (
     <div>
       <Header/>
-      <div className='app_container border border-info'>
+      <div className='app__container border border-info'>
         <SideBar/>
-      <Container fluid className='app_main border border-warning'>
+      <Container fluid className='app__main border border-warning'>
         <HomeScreen/>
       </Container>
       </div>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import './_header.scss';
 
+import { FaBars } from 'react-icons/fa';
+import { AiOutlineSearch } from 'react-icons/ai';
+import { MdNotifications, MdApps } from 'react-icons/md';
+
 const Header = () => {
   return (
-    <div className='border border-dark' >
-      Header
+    <div className='border border-dark header' >
+      <FaBars classname='header__menu' size={26} />
+
+      <img src="https://as2.ftcdn.net/v2/jpg/03/22/05/03/1000_F_322050377_yW8AC9NF5q3m9cB1aYw2n0HBWcmozJDo.jpg" 
+        alt="" className='header__logo'/>
+      <form>
+        <input type="text" placeholder='Search'/>
+        <button type='submit'><AiOutlineSearch size={22}/></button>
+      </form> 
+      <div className='header__icons'>
+        <MdNotifications size={28}/>
+        <MdApps size={28}/>
+        <img src="https://static.vecteezy.com/ti/gratis-vektor/p1/14212681-weibliches-benutzerprofil-avatar-ist-eine-frau-eine-figur-fur-einen-bildschirmschoner-mit-emotionen-fur-das-design-von-websites-und-mobilen-appsillustration-auf-einem-weissen-getrennten-hintergrund-vektor.jpg" 
+          alt="avatar" />
+      </div>
     </div>
   )
 }
